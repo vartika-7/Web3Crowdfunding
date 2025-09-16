34b60c0e0f0b6fa1ce2f3260490b03b9e23ec884
vartika-7 <159522191+vartika-7@users.noreply.github.com>
2025-09-16 19:46:01 +0530

    Update index.jsx

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -7,7 +7,7 @@ import { EditionMetadataWithOwnerOutputSchema } from '@thirdweb-dev/sdk';
 const StateContext = createContext();
 
 export const StateContextProvider = ({ children }) => {
-  const { contract } = useContract('0xf59A1f8251864e1c5a6bD64020e3569be27e6AA9');
+  const { contract } = useContract('0x1e9686fb35e91f5056c8fc2d8b54578fd33c0ee3');
   const { mutateAsync: createCampaign } = useContractWrite(contract, 'createCampaign');
 
   const address = useAddress();
